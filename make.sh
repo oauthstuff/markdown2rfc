3ecf2ff5e6fac95a903d882a117368eb8d4fc654
Daniel Fett <danielf@yes.com>
2019-09-23 10:02:42 +0200

    Fix regex

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILENAME=`grep value main.md | cut -d'"' -f2`
+FILENAME=`grep seriesInfo main.md -A 10 | grep value -m 1 | cut -d'"' -f2`
 
 mmark -2 main.md > $FILENAME.xml
 `which xml2rfc` --legacy --html $FILENAME.xml
